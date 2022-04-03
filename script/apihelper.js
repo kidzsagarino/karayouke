d9d36e7a7a35bc3d7e52ddfcb74821df14e49a17
kidzsagarino <khean92@gmail.com>
2022-04-03 09:59:08 +0800

    updated things

--- script/apihelper.js
+++ script/apihelper.js
@@ -1,5 +1,7 @@
 import sngr from './sngr.me.js'
 
+const apiKey = "AIzaSyB3TULQ3VALh7ugd-wamkIKQSo1oNcg5tc"; //window.localStorage.getItem('YDAPIKey');
+
 async function fetchDataGet(url){
     return await fetch(url,{
         headers: {
@@ -22,12 +24,12 @@ async function fetchDataGet(url){
 export default {
     search: async function(q){
 
-         return await fetchDataGet(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${q.replace('karaoke', '') + ' karaoke'}&type=video&videoEmbeddable=true&key=${window.localStorage.getItem('YDAPIKey')}`);
+         return await fetchDataGet(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${q.replace('karaoke', '') + ' karaoke'}&type=video&videoEmbeddable=true&key=${apiKey}`);
    
       
     },
     video: async function(id){
-        return await fetchDataGet(`https://www.googleapis.com/youtube/v3/videos?id=${id}&part=contentDetails&key=${window.localStorage.getItem('YDAPIKey')}`).catch(function(err){
+        return await fetchDataGet(`https://www.googleapis.com/youtube/v3/videos?id=${id}&part=contentDetails&key=${apiKey}`).catch(function(err){
             console.log(err);
            
         });
