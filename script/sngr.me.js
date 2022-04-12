dd924145e4510204c7469637c8fa87bf499f8d4e
kidzsagarino <khean92@gmail.com>
2022-04-12 22:50:46 +0800

    changed layout

--- script/sngr.me.js
+++ script/sngr.me.js
@@ -43,7 +43,7 @@ var app = {
             buttonReserve.setAttribute('data-id', videoID);
             buttonReserve.setAttribute('data-title', title);
             buttonReserve.setAttribute('data-duration', videoDuration);
-            buttonReserve.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="20px"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="#fff" d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z"/></svg>';
+            buttonReserve.innerHTML = 'RESERVE';
 
             buttonReserve.addEventListener('click', function(e){
                 let song = {
@@ -53,17 +53,15 @@ var app = {
                 }
                 
                 
-                
+                buttonReserve.innerHTML = "+1 SONG!";
+                buttonReserve.classList.add('success-reserved');
                 self.reserveSong(song);
 
-                const sucessSpan = document.createElement('span');
-                sucessSpan.classList.add('success-reserved');
-
-                sucessSpan.innerHTML = "This song was added to the list."
-                topDiv.appendChild(sucessSpan);
+                
                 setTimeout(function(){
-                    sucessSpan.remove();
-                }, 1500);
+                    buttonReserve.innerHTML = "RESERVE";
+                    buttonReserve.classList.remove('success-reserved');
+                }, 1000);
 
                 callback();
         
